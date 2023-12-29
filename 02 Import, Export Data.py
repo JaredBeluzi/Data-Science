a12369e08aab30a3929c51e53f9f787326b5445c
Jared Beluzi <126320399+JaredBeluzi@users.noreply.github.com>
2023-12-29 12:20:48 +0100

    Create 02 Import, Export Data.py

---
+++ 02 Import, Export Data.py
@@ -0,0 +1,25 @@
+
+# MS SQL Server --> DataFrame
+
+import pandas as pd
+import pyodbc
+import sqlalchemy as sa
+import os
+
+DRIVER = "ODBC Driver 18 for SQL Server"
+HOST = "XX.XX.XXX.XXX"
+DATABASE = "Ergebnisse"
+USER = "user_name"
+PASSWORD = "password"  # you can use os.environ['user_pwd'] to put this part into a local file
+
+engine_url = sa.engine.URL.create(
+        "mssql+pyodbc",
+        host=HOST,
+        username=USER,
+        password=PASSWORD,
+        database=DATABASE,
+        query=dict(driver=DRIVER, TrustServerCertificate='yes')
+    )
+engine = sa.create_engine(engine_url, future=True, fast_executemany=True)
+
+df = pd.read_sql(f"SELECT * FROM S_12345", engine)
