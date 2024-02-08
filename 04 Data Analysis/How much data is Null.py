e0ae759ce3dc4e3d61b019321d704699bfd00fdc
Jared Beluzi <126320399+JaredBeluzi@users.noreply.github.com>
2024-02-08 10:36:15 +0100

    Update How much data is Null.py

--- 04 Data Analysis/How much data is Null.py
+++ 04 Data Analysis/How much data is Null.py
@@ -6,3 +6,11 @@ df.isnull().sum()/df.shape[0]*100
 
 # visualization
 sb.heatmap(df.isnull(), yticklabels=False, cbar=False, cmap='viridis')
+
+# drop useless cols
+df.drop(['col1', 'col2'],axis=1,inplace=True)
+
+# drop rows with missing data
+df.dropna(inplace=True)
+
+# if you need the cols or rows, you should impute the missing data with realistic fake data
