edfce0119f7e60d282d680f151acfe456a40bb1a
Jared Beluzi <126320399+JaredBeluzi@users.noreply.github.com>
2024-02-08 10:31:22 +0100

    Update and rename How good are cols filled in df.py to How much data is Null.py

--- 04 Data Analysis/How much data is Null.py
+++ 04 Data Analysis/How much data is Null.py
@@ -1,3 +1,8 @@
 import seaborn as sb
+import pandas as pd
 
+# output as list
+df.isnull().sum()/df.shape[0]*100
+
+# visualization
 sb.heatmap(df.isnull(), yticklabels=False, cbar=False, cmap='viridis')
