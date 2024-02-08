481c56488f9b81c3468aace9f3524aff36e64a89
Jared Beluzi <126320399+JaredBeluzi@users.noreply.github.com>
2024-02-08 10:01:50 +0100

    Update Lineplot.py

--- 06 Visualization/Lineplot.py
+++ 06 Visualization/Lineplot.py
@@ -2,9 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0,5,11) # 0, 0.5, 1, ..., 4.5, 5
+# generate data
+x = np.linspace(0,5,101) # 0, 0.05, 0.1, ..., 4.95, 5
 y1 = x**2
 y2 = np.sin(x)
+
+# plot
 plt.plot(x, y1, 'r-', label='f(x)=x^2')
 plt.plot(x, y2, 'r-', label='f(x)=sin(x)')
 plt.xlabel('x-axis')
