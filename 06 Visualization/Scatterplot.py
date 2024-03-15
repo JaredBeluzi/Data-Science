40f73779651eae89a226cd9e9a58149852ae113a
Jared Beluzi <126320399+JaredBeluzi@users.noreply.github.com>
2024-03-15 11:36:37 +0100

    added figsize

--- 06 Visualization/Scatterplot.py
+++ 06 Visualization/Scatterplot.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 x = np.random.normal(5.0, 1.0, 1000)
 y = np.random.normal(10.0, 2.0, 1000)
 
+# plot
+plt.figure(figsize=(14, 6))
 plt.scatter(x, y)
 plt.xlabel('x-axis')
 plt.ylabel('y-axis')
