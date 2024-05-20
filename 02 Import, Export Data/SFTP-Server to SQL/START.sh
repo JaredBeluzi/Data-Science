57de1042d983d022a2a2081004459fae769eaf5c
Jared Beluzi <126320399+JaredBeluzi@users.noreply.github.com>
2024-05-20 11:13:58 +0200

    renamed yaml

--- 02 Import, Export Data/SFTP-Server to SQL/START.sh
+++ 02 Import, Export Data/SFTP-Server to SQL/START.sh
@@ -1,1 +1,1 @@
-/usr/bin/python3 /var/dscode/svcDS/DavasoTP5/SFTP_DataTransfer.py --config-filepath /var/dscode/svcDS/DavasoTP5/davaso_sftpconfig.yml
+/usr/bin/python3 /var/dscode/svcDS/DavasoTP5/SFTP_DataTransfer.py --config-filepath /var/dscode/svcDS/DavasoTP5/config.yml
