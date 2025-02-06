8ae680c1561ca8e7465fcd7c3858608b88e946ea
Jared Beluzi <126320399+JaredBeluzi@users.noreply.github.com>
2025-02-06 15:35:10 +0100

    Delete 03 Data Transformation/remove data.py

--- 03 Data Transformation/remove data.py
+++
@@ -1,8 +0,0 @@
-# remove cols
-df.drop(['col1', 
-         'col2', 
-         'col3'], axis=1, inplace=True)
-
-# remove after date
-cutoff_date = datetime.date(2023, 12, 25)
-df = df.truncate(after=cutoff_date, copy=False)
