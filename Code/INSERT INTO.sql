5257f81d87506d1b203ffb612dd2306b514f0376
Jared Beluzi <126320399+JaredBeluzi@users.noreply.github.com>
2025-02-06 16:56:05 +0100

    Update INSERT INTO.sql

--- Code/INSERT INTO.sql
+++ Code/INSERT INTO.sql
@@ -14,3 +14,18 @@ SELECT
 FROM dbo.Person AS p
 INNER JOIN  dbo.Name AS n
   ON p.ID = n.ID
+
+----------------------------
+---- INSERT INTO VALUES ----
+----------------------------
+    
+-- Code fügt manuell Zeilen in Tabelle hinzu
+
+INSERT INTO dbo.Namen (
+    Vorname
+,   Nachname
+) -- Hier oben alle Spalten explizit nennen. Sonst kann Spaltenreihenfolge durcheinander kommen beim Befüllen der Spalten.
+VALUES
+  ('Peter', 'Arndt')
+, ('Luisa', 'Scharafinski')
+, ('Robert', 'Ullrich')
