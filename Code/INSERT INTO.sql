d2e72c7a72fbc0e62e56ea16db11e99bc9bfb1c2
Jared Beluzi <126320399+JaredBeluzi@users.noreply.github.com>
2025-02-06 16:55:10 +0100

    Create INSERT INTO.sql

---
+++ Code/INSERT INTO.sql
@@ -0,0 +1,16 @@
+----------------------------
+---- INSERT INTO SELECT ----
+----------------------------
+
+-- Code fügt Zeilen aus Select in Tabelle dbo.Personen hinzu
+
+INSERT INTO dbo.Personen (
+    Vorname
+,   Nachname
+) -- Man sollte hier wie oben nochmal die Spalten aufliste. Sonst kann es zu Vertauschungen bei der Spaltenreihenfolge kommen, wenn in diese geschrieben wird.
+SELECT 
+    n.Vorname
+,   n.Nachname
+FROM dbo.Person AS p
+INNER JOIN  dbo.Name AS n
+  ON p.ID = n.ID
