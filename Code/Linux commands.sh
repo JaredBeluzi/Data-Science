a7473b8e259582199cf42101d53b332195957b80
Jared Beluzi <126320399+JaredBeluzi@users.noreply.github.com>
2025-02-07 11:40:49 +0100

    Create Linux commands.sh

---
+++ Code/Linux commands.sh
@@ -0,0 +1,23 @@
+# list all objects in current dir
+ls
+
+# change directory to dir / go one level up in hierarchy
+cd <dir>
+cd ..
+
+# create / remove file.xy
+touch <file.xy>
+rm <file.xy>
+
+# install / uninstall package
+apt-get install <package>
+apt-get remove <package>
+
+# update all packages
+sudo apt-get update && apt-get upgrade
+
+# show task manager
+htop
+
+# restart system
+sudo reboot
