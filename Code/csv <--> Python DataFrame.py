d099558dfb8a9f113f0584780d94066a1b813f5c
Jared Beluzi <126320399+JaredBeluzi@users.noreply.github.com>
2025-09-03 14:26:49 +0200

    Update csv <--> Python DataFrame.py

--- Code/csv <--> Python DataFrame.py
+++ Code/csv <--> Python DataFrame.py
@@ -4,4 +4,4 @@ import pandas as pd
 df = pd.read_csv('path\filename.csv')
 
 # DataFrame to csv
-df.to_csv('path\filename.csv' encoding='utf-8', index=False)
+df.to_csv('path\filename.csv', encoding='utf-8', index=False)
